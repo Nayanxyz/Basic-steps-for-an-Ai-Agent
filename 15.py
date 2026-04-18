e98445953df1f7a76378323629684530cdf5a983
NayanPython <choudharyji527@gmail.com>
2026-04-18 16:36:11 +0530

    15.2

--- 15.py
+++ 15.py
@@ -6,6 +6,13 @@ print("type 'exit' to stop" )
 while True:
     user_input = input("You: ")
 
+    if user_input == "exit":
+        network_payload = js.dumps(chat_history, indent=4)
+
+        print("Sending payload to server...")
+        print(network_payload)
+
+        break
 
     dictionary = {"role": "user", "content": user_input}
 
