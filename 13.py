0d8740a3ab2d8e79d1de3877c6abde7eca411429
NayanPython <choudharyji527@gmail.com>
2026-04-18 16:34:49 +0530

    13.1

---
+++ 13.py
@@ -0,0 +1,16 @@
+import json as js
+
+broken_ai_string = '{"action": "multiply", "parameters": [12, 4}'
+
+def multi(a,b):
+    multiplication = a * b
+    return multiplication
+
+
+def safe_parse(json_string):
+    try:
+        parsed = js.loads(json_string)
+        return parsed
+    except js.JSONDecodeError:
+        print("Warning: AI generated invalid JSON.")
+        return {"action": "error", "parameters": [0, 0]}
