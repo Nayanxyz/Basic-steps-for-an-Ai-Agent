638f6da9e69025e2ddb505ff1a059784d3e89d18
NayanPython <choudharyji527@gmail.com>
2026-04-21 14:33:37 +0530

    27.2.A-streamlit

--- 27-streamlit.py
+++ 27-streamlit.py
@@ -11,3 +11,8 @@ for message in st.session_state.chat_history:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
+# 3. Listen for new user input (The ':=' is called a Walrus Operator, it assigns and checks at the same time)
+if user_input := st.chat_input("Type your message..."):
+    # A. Instantly draw the user's message on the screen
+    with st.chat_message("user"):
+        st.write(user_input)
