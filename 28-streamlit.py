e098ecd6f8434390098a69248345c420688181ce
NayanPython <choudharyji527@gmail.com>
2026-04-21 14:36:23 +0530

    28.2-streamlit

--- 28-streamlit.py
+++ 28-streamlit.py
@@ -14,3 +14,17 @@ API_KEY = os.getenv("GROQ_API_KEY")
 CLOUD_URL = "https://api.groq.com/openai/v1/chat/completions"
 
 
+def send_to_cloud_ai(history_list):
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "model": "llama-3.1-8b-instant",
+        "messages": history_list,
+        "temperature": 0.7
+    }
+    response = requests.post(CLOUD_URL, headers=headers, json=payload)
+    return response.json()["choices"][0]["message"]["content"]
+
+
