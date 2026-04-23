df801307b539109cc0a86939804bad62aa5f4f20
NayanPython <choudharyji527@gmail.com>
2026-04-23 12:56:13 +0530

    34.2-Groq-Scraper-Gradio

--- 34-merger-Groq-Scraper.py
+++ 34-merger-Groq-Scraper.py
@@ -17,4 +17,61 @@ API_KEY = os.getenv("GROQ_API_KEY")
 CLOUD_URL = "https://api.groq.com/openai/v1/chat/completions"
 
 
+def send_to_local_ai(history_list):
+    # 2. Add the Headers (Authentication)
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "model": "llama-3.1-8b-instant",  # Groq's cloud model name
+        "messages": history_list,
+        "temperature": 0.7
+    }
+
+
+    server_response = requests.post(CLOUD_URL, headers=headers, json=payload)
+
+    response_data = server_response.json()
+
+    #print("RAW SERVER RESPONSE:", response_data) for the debug
+
+    # Groq (and OpenAI) bury the text slightly differently than Ollama:
+    ai_text = response_data["choices"][0]["message"]["content"]
+
+    return ai_text
+
+def fetch_weather(city):
+    return f"It is 75 degrees and sunny in {city}."
+
+
+def scrape_wikipedia(topic):
+    # 1. SANITIZE THE DATA: Replace all spaces with underscores
+    clean_topic = topic.replace(" ", "_")
+
+    # 2. DEBUGGER: Print it to the terminal so we can watch it work
+    print(f"\n--- SCRAPER TRIGGERED: Searching for '{clean_topic}' ---\n")
+
+    url = f"https://en.wikipedia.org/wiki/{clean_topic}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+
+    # 2. Pass the headers into the get request
+    response = requests.get(url, headers=headers)
+
+    article = response.text
+
+    soup = BeautifulSoup(article, 'html.parser')
+
+    content = soup.find_all('p')
+
+    massive_string = ""
+    for paragraph in content:
+        massive_string += paragraph.get_text() + "\n"
+
+    return massive_string[:2000]
+
+
 
