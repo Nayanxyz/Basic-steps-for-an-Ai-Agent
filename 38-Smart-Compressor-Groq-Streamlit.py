1443bf7478de25fdabc723767693c163460dfa18
NayanPython <choudharyji527@gmail.com>
2026-04-25 10:04:07 +0530

    38.1-Updated-Compressor-Streamlit-Groq

---
+++ 38-Smart-Compressor-Groq-Streamlit.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import os
+import requests
+import json as js
+import chromadb
+import re
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+
+load_dotenv()
+
+# ==========================================
+# 1. THE BRAIN & TOOLS
+# ==========================================
+API_KEY = os.getenv("GROQ_API_KEY")
+CLOUD_URL = "https://api.groq.com/openai/v1/chat/completions"
+
+
+def fetch_weather(city):
+    return f"It is 75 degrees and sunny in {city}."
+
+
+def send_to_cloud_ai(history_list):
+    headers = {"Authorization": f"Bearer {API_KEY}", "Content-Type": "application/json"}
+    payload = {"model": "llama-3.1-8b-instant", "messages": history_list, "temperature": 0.7}
+    response = requests.post(CLOUD_URL, headers=headers, json=payload)
+    return response.json()["choices"][0]["message"]["content"]
+
+def compress_memory(history_list):
+    compression_payload = [
+        {"role": "system", "content": "You are a backend memory manager. Read the conversation log and generate a detailed 'Running Fact Sheet'. You MUST retain all specific facts, user names, locations, and past actions. Do not leave out any details."},
+        {"role": "user", "content": f"Here is the raw conversation log: {str(history_list)}"}
+    ]
+    return send_to_cloud_ai(compression_payload)
+
